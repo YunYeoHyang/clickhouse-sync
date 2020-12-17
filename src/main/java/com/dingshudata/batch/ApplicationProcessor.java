063c2f0c05b4e7b6dc9c3160200e459b744a4f3e
YunYeoHyang <1016789542@qq.com>
2020-12-17 16:30:24 +0800

    init

--- src/main/java/com/dingshudata/batch/ApplicationProcessor.java
+++ src/main/java/com/dingshudata/batch/ApplicationProcessor.java
@@ -1,12 +1,16 @@
 package com.dingshudata.batch;
 
 import com.dingshudata.entity.Application;
+import com.dingshudata.entity.DateField;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.cglib.beans.BeanMap;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Field;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 
 @StepScope
@@ -20,16 +24,31 @@ public class ApplicationProcessor implements ItemProcessor<Application, LinkedHa
         for (Field field : Application.class.getDeclaredFields()) {
             String fieldName = field.getName();
             Object value = m.get(field.getName());
+            Field[] fields = DateField.class.getDeclaredFields();
             if (value == null) {
-                if (field.getType().equals(Integer.class) || field.getType().equals(Double.class)) {
-                    linkedHashMap.put(fieldName, 0);
+                if (field.getType().equals(String.class)) {
+                    if (Arrays.stream(fields).anyMatch(f -> field.getName().equals(f.getName()))) {
+                        linkedHashMap.put(fieldName, LocalDate.parse("0001-01-01", DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+                    } else {
+                        linkedHashMap.put(fieldName, "");
+                    }
                 } else {
-                    linkedHashMap.put(fieldName, "");
+                    linkedHashMap.put(fieldName, 0);
                 }
             } else {
-                linkedHashMap.put(fieldName, value);
+                if (Arrays.stream(fields).anyMatch(f -> field.getName().equals(f.getName()))) {
+                    if (value.equals("")) {
+                        linkedHashMap.put(fieldName, LocalDate.parse("0001-01-01", DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+                    } else {
+                        linkedHashMap.put(fieldName, LocalDate.parse(value.toString(), DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+                    }
+                } else {
+                    linkedHashMap.put(fieldName, value);
+                }
             }
         }
+
+        System.out.println(linkedHashMap.toString());
         return linkedHashMap;
     }
 }
---
+++ src/main/java/com/dingshudata/entity/DateField.java
@@ -0,0 +1,37 @@
+package com.dingshudata.entity;
+
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class DateField {
+    private Date acctdt;
+    private Date vip_time;
+    private Date open_mindate;
+    private Date dq_open_mindate;
+    private Date webbank_open_date;
+    private Date mobilebank_open_date;
+    private Date credit_c_open_date;
+    private Date lc_open_date;
+    private Date jj_open_date;
+    private Date gz_open_date;
+    private Date bx_open_date;
+    private Date dfgz_date_recent;
+    private Date dfsb_date_recent;
+    private Date gdhk_date_recent;
+    private Date etc_open_date;
+    private Date webbank_active_date;
+    private Date mobilebank_active_date;
+    private Date credit_c_activiate_date;
+    private Date debit_c_close_date;
+    private Date dq_close_date;
+    private Date wy_close_date;
+    private Date mobilebank_close_date;
+    private Date credit_c_close_date;
+    private Date dq_due_date_recent;
+    private Date lc_due_date_recent;
+    private Date jj_due_date_recent;
+    private Date gz_due_date_recent;
+    private Date bx_due_date_recent;
+}
